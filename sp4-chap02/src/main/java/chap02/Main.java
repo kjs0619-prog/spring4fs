ca0b766fa24e838da74a2f7829e2fcebba9eafe3
kjs0619-prog <84962654+kjs0619-prog@users.noreply.github.com>
2021-06-08 18:36:15 +0900

    커밋하기 20210608

--- sp4-chap02/src/main/java/chap02/Main.java
+++ sp4-chap02/src/main/java/chap02/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
 		Greeter g = ctx.getBean("greeter", Greeter.class);
-		String msg = g.greet("스프링");
+		String msg = g.greet("�뒪�봽留�");
 		System.out.println(msg);
 		ctx.close();
 	}
